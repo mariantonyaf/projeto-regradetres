d7b5493ee1aaa8974a09e2dce4ede9e3195403a7
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-07 13:44:40 -0300

    Update script.js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,14 @@
 document.getElementById('calculate').addEventListener('click', function(){
-    const num1 = 
+    const num1 = parseFloat(document.getElementById('num1').value)
+    const num2 = parseFloat(document.getElementById('num2').value)
+    const num3 = parseFloat(document.getElementById('num3').value)
+
+    if(isNaN(num1) || isNaN(num2) || isNaN(num3) === 0){
+        document.getElementById('result').textContent = 'Inválido!'
+        return 
+    }
+
+    let result = (num3 * num2) / num1
+
+    document.getElementById('result').textContent = result
 })
